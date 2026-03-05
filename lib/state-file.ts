7e5757d09fbf4c518f1a09985df16ce46d89bb12
ChristopherDedominici <18092467+ChristopherDedominici@users.noreply.github.com>
2026-03-05 15:18:40 +0000

    undo some changes

--- lib/state-file.ts
+++ lib/state-file.ts
@@ -276,7 +276,7 @@ function _getStateFileFileName(networkStateFile = "") {
 }
 
 function _getFileName(dir: string, networkName: string, prefix: string = NETWORK_STATE_FILE_PREFIX) {
-  return resolve(dir, `${prefix}${networkName}-${process.pid}.json`);
+  return resolve(dir, `${prefix}${networkName}.json`);
 }
 
 function _readStateFile(fileName: string) {
